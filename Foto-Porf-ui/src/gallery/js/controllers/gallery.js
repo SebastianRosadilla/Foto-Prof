5fdeaae0502479bf7dc2d3c01f241e4d6823016c
Alfredo Sebastian Rosadilla Ribeiro <sebafing@hotmail.com>
2015-11-15 13:51:35 -0300

    Start with ing API

--- Foto-Porf-ui/src/gallery/js/controllers/gallery.js
+++ Foto-Porf-ui/src/gallery/js/controllers/gallery.js
@@ -1,18 +1,25 @@
 (function (ng) {
   'use strict';
 
-  var GalleryCtrl = function ($state) {
+  var GalleryCtrl = function ($state, $document) {
     this._$state = $state;
+    this._$document = $document;
 
     // Run the galleries
     this.galleries();
 
+    //Prints
+    this.obteinPrints();
   };
 
   GalleryCtrl.prototype.galleries = function () {
     new Photostack( document.getElementById( 'photostack-1' ));
     new Photostack( document.getElementById( 'photostack-2' ));
     new Photostack( document.getElementById( 'photostack-3' ));
+  };
+
+  GalleryCtrl.prototype.obteinPrints = function () {
+    console.log(this);
   };
 
   ng.module('foto-prof')
